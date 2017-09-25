064e80b727bf567e18332b5739f69fd1e6116dc6
Mats Dale <14minerboe16@gmail.com>
2017-09-25 16:12:34 -0600

    Add tests for ice_cream_party?

--- test.rb
+++ test.rb
@@ -17,4 +17,25 @@ describe 'Methods2' do
     end
 
   end
+
+  describe 'ice_cream_party?' do
+    it 'no ice cream but lots of candy' do
+      ice_cream_party?(0, 100).must_equal(0)
+    end
+    it 'no candy but lots of ice cream' do
+      ice_cream_party?(100, 0).must_equal(0)
+    end
+    it 'great party - tons of food' do
+      ice_cream_party?(10, 30).must_equal(2)
+    end
+    it 'candy is double ice cream' do
+      ice_cream_party?(10, 20).must_equal(2)
+    end
+    it 'good party - over 5' do
+      ice_cream_party?(6, 6).must_equal(1)
+    end
+    it 'good party - equals 5' do
+      ice_cream_party?(5, 5).must_equal(1)
+    end
+  end
 end
