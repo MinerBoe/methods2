b7e411382a0f9bba5b543d2ea246bb292cdbb525
Mats Dale <14minerboe16@gmail.com>
2017-09-25 13:42:00 -0600

    Add test for elevenish?

--- test.rb
+++ test.rb
@@ -1,9 +1,20 @@
-require 'test_setup'
+require './test_setup'
 require './methods2'
 
 
 describe 'Methods2' do
 
   # TODO - write tests for each method in methods2.rb
-  
+  describe 'elevenish?' do
+    it 'equal to eleven' do
+      elevenish?(11).must_equal(true)
+    end
+    it 'perfect multiple of 11' do
+      elevenish?(22).must_equal(true)
+    end
+    it 'one greater than a multiple of eleven' do
+      elevenish?(23).must_equal(true)
+    end
+
+  end
 end
